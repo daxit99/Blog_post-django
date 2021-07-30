05b1ff66574d5600b9c25f310cfdcadbfc6cf0c2
daxit99 <73168093+daxit99@users.noreply.github.com>
2021-07-31 04:17:11 +0500

    Added django-heroku

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os   
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,6 @@ EMAIL_PORT= 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER ='daku95109@gmail.com'
 EMAIL_HOST_PASSWORD = 'daxit@7122'
+
+
+django_heroku.settings(locals())
