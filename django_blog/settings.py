7dd4ed06777b11b246825ad8752685532a52ddf6
daxit99 <73168093+daxit99@users.noreply.github.com>
2021-07-31 04:09:37 +0500

    Updated Settings

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p#0k7f8jq$f$@rb$s87dl0(&0^+-g)^%k+6!^#a$7gw+5ie7&u'
+# SECRET_KEY = 'django-insecure-p#0k7f8jq$f$@rb$s87dl0(&0^+-g)^%k+6!^#a$7gw+5ie7&u'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['daxitblog.herokuapp.com']
 
 
 # Application definition
